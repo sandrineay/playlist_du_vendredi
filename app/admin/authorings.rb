cd140ed10496d5f0f7ef72a0f17a7464d0a87c03
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-03-10 18:56:18 +0100

    Arrange order of dropdowns in ActiveAdmin

--- app/admin/authorings.rb
+++ app/admin/authorings.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Authoring do
   permit_params :song_id, :artist_id
+
+  form do |f|
+    f.input :song, collection: Song.order(title: :asc)
+    f.input :artist, collection: Artist.order(name: :asc)
+    f.actions
+  end
 end
--- app/admin/belongings.rb
+++ app/admin/belongings.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Belonging do
   permit_params :playlist_id, :category_id, :song_id
+
+  form do |f|
+    f.input :playlist, collection: Playlist.order(title: :asc)
+    f.input :category, collection: Category.order(name: :asc)
+    f.input :song, collection: Song.order(title: :asc)
+    f.actions
+  end
 end
